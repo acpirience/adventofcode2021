8b2bd115ee3653e6f617fb1333fd78855909a592
acpirience <acpirience@gmail.com>
2021-12-05 23:25:22 +0100

    prepare day 4

---
+++ day4/day4pt1.py
@@ -0,0 +1,21 @@
+from loguru import logger
+
+
+def main():
+    """
+    https://adventofcode.com/2021/day/4
+    """
+    day = "4"
+    part = "1"
+
+    logger.info(f"Day {day} part {part}")
+
+    # load input
+    with open("day" + day + "\\day" + day + "input.txt", "r") as inputFile:
+        pass
+
+    logger.info(f"Day {day} part {part} result: ")
+
+
+if __name__ == "__main__":
+    main()
