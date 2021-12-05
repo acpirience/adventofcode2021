87e7a10d68d6e5104716241679683d6fb45d18e7
acpirience <acpirience@gmail.com>
2021-12-05 20:39:47 +0100

    prepare day 2

---
+++ day2/day2pt1.py
@@ -0,0 +1,20 @@
+from loguru import logger
+
+
+def main():
+    """
+    https://adventofcode.com/2021/day/2
+    """
+    day = "2"
+    part = "1"
+
+    logger.info(f"Day {day} part {part}")
+    # load input
+    with open("day" + day + "\\day" + day + "exampleinput.txt", "r") as inputFile:
+        pass
+
+    logger.info(f"Day {day} part {part} result: ")
+
+
+if __name__ == "__main__":
+    main()
