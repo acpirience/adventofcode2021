1d5d75d17d81029d63d1cec9543f2146e652f2f9
acpirience <acpirience@gmail.com>
2021-12-05 22:20:02 +0100

    Day 3 part 1 ok

--- day3/day3pt1.py
+++ day3/day3pt1.py
@@ -10,12 +10,30 @@ def main():
 
     logger.info(f"Day {day} part {part}")
 
+    accum = []
+    tot = 0
     # load input
-    with open("day" + day + "\\day" + day + "exampleinput.txt", "r") as inputFile:
+    with open("day" + day + "\\day" + day + "input.txt", "r") as inputFile:
         for line in inputFile.readlines():
-            logger.info(line.rstrip())
+            line = str(line.rstrip())
+            tot += 1
 
-    logger.info(f"Day {day} part {part} result: ")
+            if not accum:
+                accum = [int(line[x]) for x in range(len(line))]
+            else:
+                for bit in range(len(line)):
+                    accum[bit] = accum[bit] + int(line[bit])
+
+    thresh = tot // 2
+    gamma = "".join(["1" if x > thresh else "0" for x in accum])
+    epsilon = "".join(["0" if x > thresh else "1" for x in accum])
+
+    gamma_rate = int(gamma, base=2)
+    epsilon_rate = int(epsilon, base=2)
+
+    logger.info(
+        f"Day {day} part {part} result: {gamma_rate}-{epsilon_rate} => {gamma_rate * epsilon_rate}"
+    )
 
 
 if __name__ == "__main__":
