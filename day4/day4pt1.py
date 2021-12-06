ba72b8079f7107cb85b8f86183e33ad4e178ec2b
acpirience <acpirience@gmail.com>
2021-12-06 10:07:19 +0100

    Day 4 part 1 ok

--- day4/day4pt1.py
+++ day4/day4pt1.py
@@ -1,4 +1,5 @@
 from loguru import logger
+from bingo import Bingo
 
 
 def main():
@@ -10,11 +11,48 @@ def main():
 
     logger.info(f"Day {day} part {part}")
 
+    drawn_numbers = []
+    nb_cards = 0
+    tmp_card = []
+    card_list = []
     # load input
     with open("day" + day + "\\day" + day + "input.txt", "r") as inputFile:
-        pass
+        drawn_numbers = inputFile.readline().rstrip().split(",")
+        lines = inputFile.readlines()
+        for line in lines:
+            line = line.rstrip()
+            if line == "":  # Start new card, end previous
+                if nb_cards != 0:
+                    card_list.append(Bingo(tmp_card))
+                    tmp_card = []
+                nb_cards += 1
+            else:
+                tmp_card.append(line.rstrip().split())
 
-    logger.info(f"Day {day} part {part} result: ")
+        # store last card
+        card_list.append(Bingo(tmp_card))
+
+    is_bingo = False
+    winner = None
+    number_winner = None
+    for number in drawn_numbers:
+        logger.info(f"checking for number {number}")
+        for card in card_list:
+            card.mark(number)
+            if card.check_for_line():
+                logger.info(f"Bingo !")
+                is_bingo = True
+                winner = card
+                number_winner = number
+                break
+        if is_bingo:
+            break
+
+    logger.info(f"\n{winner}")
+
+    logger.info(
+        f"Day {day} part {part} result: {winner.calculate_Score(number_winner)}"
+    )
 
 
 if __name__ == "__main__":
