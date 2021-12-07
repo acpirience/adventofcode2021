60eb55999942746634437d9498126898b9d819fd
acpirience <acpirience@gmail.com>
2021-12-07 09:17:19 +0100

    Day 7 part 2 ok

--- day7/day7pt2.py
+++ day7/day7pt2.py
@@ -5,6 +5,13 @@ def str_array_to_int(pt):
     return [int(x) for x in pt]
 
 
+def compute_fuel(x, y):
+    ret = 0
+    for i in range(1, abs(x - y) + 1):
+        ret += i
+    return ret
+
+
 def main():
     """
     https://adventofcode.com/2021/day/7#part2
@@ -16,9 +23,23 @@ def main():
 
     # load input
     with open("day" + day + "\\day" + day + "input.txt", "r") as inputFile:
-        pass
+        positions = str_array_to_int(inputFile.readline().split(","))
 
-    logger.info(f"Day {day} part {part} result: ")
+    min_fuel = None
+    best_position = None
+    for x in range(min(positions), max(positions) + 1):
+        curr_fuel = 0
+        for position in positions:
+            curr_fuel += compute_fuel(position, x)
+        if min_fuel is None or curr_fuel < min_fuel:
+            min_fuel = curr_fuel
+            best_position = x
+
+        logger.info(
+            f"pos {x} - fuel:{curr_fuel} - min fuel:{min_fuel} - best pos:{best_position}"
+        )
+
+    logger.info(f"Day {day} part {part} result: {min_fuel}")
 
 
 if __name__ == "__main__":
