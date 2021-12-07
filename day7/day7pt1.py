cdf82ce8524965d4640ac4966121fa63da33b202
acpirience <acpirience@gmail.com>
2021-12-07 08:54:10 +0100

    Day 7 part 1 ok

--- day7/day7pt1.py
+++ day7/day7pt1.py
@@ -16,9 +16,23 @@ def main():
 
     # load input
     with open("day" + day + "\\day" + day + "input.txt", "r") as inputFile:
-        pass
+        positions = str_array_to_int(inputFile.readline().split(","))
 
-    logger.info(f"Day {day} part {part} result: ")
+    min_fuel = max(positions) * len(positions)
+    best_position = None
+    for x in range(min(positions), max(positions) + 1):
+        curr_fuel = 0
+        for position in positions:
+            curr_fuel += abs(position - x)
+        if curr_fuel < min_fuel:
+            min_fuel = curr_fuel
+            best_position = x
+
+        logger.info(
+            f"pos {x} - fuel:{curr_fuel} - min fuel:{min_fuel} - best pos:{best_position}"
+        )
+
+    logger.info(f"Day {day} part {part} result: {min_fuel}")
 
 
 if __name__ == "__main__":
