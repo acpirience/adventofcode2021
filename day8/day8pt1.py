ea7aa33118d3decf3f362c26baa99956682a8c06
acpirience <acpirience@gmail.com>
2021-12-07 09:24:34 +0100

    prepare day 8

---
+++ day8/day8pt1.py
@@ -0,0 +1,25 @@
+from loguru import logger
+
+
+def str_array_to_int(pt):
+    return [int(x) for x in pt]
+
+
+def main():
+    """
+    https://adventofcode.com/2021/day/8
+    """
+    day = "8"
+    part = "1"
+
+    logger.info(f"Day {day} part {part}")
+
+    # load input
+    with open("day" + day + "\\day" + day + "input.txt", "r") as inputFile:
+        pass
+
+    logger.info(f"Day {day} part {part} result: ")
+
+
+if __name__ == "__main__":
+    main()
