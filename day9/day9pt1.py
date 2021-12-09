7eff78fc8a3c49da46e5008d9ff1a13e2213f7f9
acpirience <acpirience@gmail.com>
2021-12-09 08:35:18 +0100

    Day 9 part 1 ok

--- day9/day9pt1.py
+++ day9/day9pt1.py
@@ -1,6 +1,10 @@
 from loguru import logger
 
 
+def str_array_to_int(pt):
+    return [int(x) for x in pt]
+
+
 def main():
     """
     https://adventofcode.com/2021/day/9
@@ -10,11 +14,36 @@ def main():
 
     logger.info(f"Day {day} part {part}")
 
+    heights = []
     # load input
     with open("day" + day + "\\day" + day + "input.txt", "r") as inputFile:
-        pass
+        for line in inputFile.readlines():
+            heights.append(str_array_to_int(line.rstrip()))
 
-    logger.info(f"Day {day} part {part} result: ")
+    logger.info(heights)
+    dim_y = len(heights[0])
+    dim_x = len(heights)
+    logger.info(f"dim_x={dim_x} dim_y={dim_y}")
+
+    result = 0
+    for x in range(dim_x):
+        for y in range(dim_y):
+            lowest = True
+            test = heights[x][y]
+            if x > 0 and test >= heights[x - 1][y]:
+                lowest = False
+            if x < (dim_x - 1) and test >= heights[x + 1][y]:
+                lowest = False
+            if y > 0 and test >= heights[x][y - 1]:
+                lowest = False
+            if y < (dim_y - 1) and test >= heights[x][y + 1]:
+                lowest = False
+
+            if lowest:
+                result += test + 1
+                logger.info(f"{x}-{y} => {test}")
+
+    logger.info(f"Day {day} part {part} result: {result}")
 
 
 if __name__ == "__main__":
